42344a714c80a0f06525ba7db4cfda9c31b0d643
Alessandro Franceschi <al@lab42.it>
2014-06-12 08:57:07 +0200

    Fixes

--- samples/09_scaling/config.ru
+++ samples/09_scaling/config.ru
@@ -6,5 +6,10 @@ ARGV << "--rack"
 ARGV << "--confdir" << "/etc/puppet"
 ARGV << "--vardir" << "/var/lib/puppet"
 require 'puppet/util/command_line'
+<% if @rubyversion[0..2].to_f >= 1.9 -%>
+# https://projects.puppetlabs.com/issues/20897
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+<% end -%>
 run Puppet::Util::CommandLine.new.execute
 
--- samples/09_scaling/passenger_setup.pp
+++ samples/09_scaling/passenger_setup.pp
@@ -1,4 +1,8 @@
 # Passenger setup on RedHat 6
+# To test this manifest run, from the directory that contains this file, and the
+# used templates:
+# puppet apply -t passenger_setup.pp --templatedir ./
+
 
 # Epel repo
 yumrepo { 'epel':
@@ -6,49 +10,49 @@ yumrepo { 'epel':
   gpgcheck   => 1,
   enabled    => 1,
   gpgkey     => 'https://fedoraproject.org/static/0608B895.txt',
-}
+} ->
 
 # Passenger repo
 yumrepo { 'passenger':
   baseurl    => 'http://passenger.stealthymonkeys.com/rhel/$releasever/$basearch',
   mirrorlist => 'http://passenger.stealthymonkeys.com/rhel/mirrors',
   enabled    => 1,
-  gpgkey     => 'http://passenger.stealthymonkeys.com/RPM-GPG-KEYstealthymonkeys.asc',
-}
+  gpgkey     => 'http://passenger.stealthymonkeys.com/RPM-GPG-KEY-stealthymonkeys.asc',
+} ->
 
 # Packages
 package { [ 'mod_passenger' , 'httpd' , 'mod_ssl' , 'rubygems']:
   ensure => present,
-}
+} ->
 
 package { 'rack':
   ensure => present,
   provider => gem,
-}
+} ->
 
-file { '/etc/httpd/conf.d/passenger.conf':
+file { '/etc/httpd/conf.d/puppetmaster.conf':
   ensure  => present,
-  content => template('passenger.conf.erb'),
-}
+  content => template('puppetmaster.conf'),
+} ->
 
 # Rack setup for Passenger
 file { ['/etc/puppet/rack','/etc/puppet/rack/public','/etc/puppet/rack/tmp']:
   ensure => directory,
   owner  => 'puppet',
   group  => 'puppet',
-}
+} ->
 
 file { '/etc/puppet/rack/config.ru':
   ensure  => present,
-  content => template('config.ru')
+  content => template('config.ru'),
   owner   => 'puppet',
   group   => 'puppet',
-}
+} ->
 
 service { 'puppetmaster':
   ensure => stopped,
   enable => false,
-}
+} ->
 
 service { 'httpd':
   ensure => running,
